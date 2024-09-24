9643a1a8a74485f33651f439311c9208808894d9
Luis França <luisflf@icloud.com>
2024-09-24 09:13:29 -0300

    deleted unecessary file.

--- prog-nucleo-f767.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openocd -s /usr/share/openocd/scripts/ --file board/stm32f7discovery.cfg --command "hla_serial 066FFF524881774867214116; program build_obdh_v0_nucleo-f767-freertos/nucleo-f767-freertos/default_cpu1/cpu1/core-cpu1 verify reset exit"
