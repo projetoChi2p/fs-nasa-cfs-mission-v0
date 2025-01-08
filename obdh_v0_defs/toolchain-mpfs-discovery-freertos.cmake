ff75bc3244c914f4e96baa55146a2cc60cb0a227
Fabio Benevenuti <fbenevenuti@inf.ufrgs.br>
2025-01-08 19:39:15 -0300

    Minor fixes on OSAL for FreeRTOS.

--- obdh_v0_defs/toolchain-mpfs-discovery-freertos.cmake
+++ obdh_v0_defs/toolchain-mpfs-discovery-freertos.cmake
@@ -211,6 +211,7 @@ add_definitions(-DBSP_MAIN_TASK_PRIORITY=150)
 add_definitions(-DFREERTOS_IDLE_TASK_STACK_SIZE_WORDS=128)
 add_definitions(-DOS_CONSOLE_TASK_REPORT_TASKS=1) # FreeRTOS tasks and stack usage
 add_definitions(-DOS_CONSOLE_TASK_REPORT_FILES=1) # FreeRTOS filesystem and files usage
+add_definitions(-DOS_ASSERT_USE_TASK_NAME=1)      # Use OSAL task name inspection during assertions.
 
 
 if(OSAL_RAMDISK_FILESYSTEM_IS_MFS)
