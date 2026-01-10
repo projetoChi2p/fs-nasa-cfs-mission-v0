c8f2031631415f99e6926d15d9a300c569baa6b1
Fabio Benevenuti <fbenevenuti@inf.ufrgs.br>
2026-01-10 17:13:47 -0300

    Minor updates on NOEL-V port for OSAL for FreeRTOS.

--- obdh_v0_defs/toolchain-riscv-noelv-freertos.cmake
+++ obdh_v0_defs/toolchain-riscv-noelv-freertos.cmake
@@ -257,7 +257,7 @@ message("+++ OSAL_SOURCE_DIR '${OSAL_SOURCE_DIR}'.")
 message("+++ CMAKE_CURRENT_BINARY_DIR '${CMAKE_CURRENT_BINARY_DIR}'.")
 
 # For NOEL-V MC-lite running at 50 MHz
-add_definitions(-DMSG_MXM_HUFF_WORK_TICKS=30)
+#add_definitions(-DMSG_MXM_HUFF_WORK_TICKS=15)
 
 # These OSAL configurations are specific to FreeRTOS and
 # have no mapping in osconfig.h.in
@@ -266,8 +266,8 @@ add_definitions(-DOS_TIMEBASE_TASK_PRIORITY=25)     # OSAL semantics, lower valu
 add_definitions(-DBSP_MAIN_TASK_STACK_SIZE_BYTES=6144)
 add_definitions(-DBSP_MAIN_TASK_PRIORITY=150)
 add_definitions(-DFREERTOS_IDLE_TASK_STACK_SIZE_WORDS=256)
-add_definitions(-DOS_CONSOLE_TASK_REPORT_TASKS=1) # FreeRTOS tasks and stack usage
-add_definitions(-DOS_CONSOLE_TASK_REPORT_FILES=1) # FreeRTOS filesystem and files usage
+#add_definitions(-DOS_CONSOLE_TASK_REPORT_TASKS=1) # FreeRTOS tasks and stack usage
+#add_definitions(-DOS_CONSOLE_TASK_REPORT_FILES=1) # FreeRTOS filesystem and files usage
 #add_definitions(-DIDLE_TASK_REPORT_TASKS=1)       # FreeRTOS tasks and stack usage
 add_definitions(-DOS_ASSERT_USE_TASK_NAME=1)      # Use OSAL task name inspection during assertions.
 #add_definitions(-DFREERTOS_TRACE_ENABLED=1)
